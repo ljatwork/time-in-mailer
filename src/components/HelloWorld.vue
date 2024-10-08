73763b81eaa926334b10600a885539e9f170c489
Leah Joy <leah_ishiguro@jtekt.co.jp>
2024-10-08 09:30:35 +0900

    Mailer

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,142 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="align-centerfill-height mx-auto" max-width="900">
+      <v-row justify="center">
+        <v-col key="1" cols="12" md="6">
+          <v-card color="primary" variant="outlined" class="mx-auto">
+            <v-card-text>
+              <div>
+                <div class="text-overline mb-1">Sign-in Email</div>
+                <div class="text-h6 mb-1">{{ signInSubject }}</div>
+                <br />
+                <div class="text-medium-emphasis">
+                  {{ signInBody }}
+                </div>
+                <br />
+                <div class="text-medium-emphasis">
+                  {{ signInSignature }}
+                </div>
+              </div>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-btn @click="generateSignInEmail">
+                Regenerate Sign-in Email
+              </v-btn>
+              <v-btn @click="openOutlookEmail" :disabled="siSendBtnDisabled">
+                Send Email
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-col key="1" cols="12" md="6">
+          <v-card color="primary" variant="outlined" class="mx-auto">
+            <v-card-text>
+              <div>
+                <div class="text-overline mb-1">Sign-out Email</div>
+                <div class="text-h6 mb-1">{{ signOutSubject }}</div>
+                <br />
+                <div class="text-medium-emphasis mb-1">
+                  {{ signOutBody }}
+                </div>
+                <br />
+                <div class="text-medium-emphasis mb-1">
+                  {{ signOutSignature }}
+                </div>
+              </div>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-btn @click="generateSignOutEmail">
+                Regenerate Sign-out Email
+              </v-btn>
+              <v-btn
+                @click="openOutlookEmail(false)"
+                :disabled="soSendBtnDisabled"
+              >
+                Send Email
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import {
+  checkInSubjects,
+  signOutSubjects,
+  checkInBodies,
+  signOutBodies,
+  MondaycheckInBodies,
+  FridaysignOutBodies,
+  signature,
+} from "@/utils/mailBody";
+
+const signInSubject = ref("");
+const signInBody = ref("");
+const signInSignature = ref("");
+const signOutSubject = ref("");
+const signOutBody = ref("");
+const signOutSignature = ref("");
+const soSendBtnDisabled = ref(true);
+const siSendBtnDisabled = ref(true);
+
+const generateSignInEmail = () => {
+  const today = new Date().getDay();
+
+  signInSubject.value =
+    checkInSubjects[Math.floor(Math.random() * checkInSubjects.length)];
+  signInSignature.value =
+    signature[Math.floor(Math.random() * signature.length)];
+  if (today === 1)
+    signInBody.value =
+      MondaycheckInBodies[
+        Math.floor(Math.random() * MondaycheckInBodies.length)
+      ];
+  else
+    signInBody.value =
+      checkInBodies[Math.floor(Math.random() * checkInBodies.length)];
+  siSendBtnDisabled.value = false;
+};
+
+const generateSignOutEmail = () => {
+  const today = new Date().getDay();
+
+  signOutSubject.value =
+    signOutSubjects[Math.floor(Math.random() * signOutSubjects.length)];
+  signOutSignature.value =
+    signature[Math.floor(Math.random() * signature.length)];
+  if (today === 5)
+    signOutBody.value =
+      FridaysignOutBodies[
+        Math.floor(Math.random() * FridaysignOutBodies.length)
+      ];
+  else
+    signOutBody.value =
+      signOutBodies[Math.floor(Math.random() * signOutBodies.length)];
+  soSendBtnDisabled.value = false;
+};
+
+const openOutlookEmail = (signIn: boolean = true) => {
+  let mailBody = `${signInBody.value}\n\n${signInSignature.value}`;
+  let subject = signInSubject.value;
+  if (!signIn) {
+    mailBody = `${signOutBody.value}\n\n${signOutSignature.value}`;
+    subject = signOutSignature.value;
+  }
+  const mailToLink = `mailto:${encodeURIComponent(
+    "masashi_yoritsune@jtekt.co.jp"
+  )}?cc=${encodeURIComponent(
+    "yusuke_okubo@jtekt.co.jp"
+  )}&subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(
+    mailBody
+  )}`;
+  window.location.href = mailToLink;
+};
+</script>
---
+++ src/utils/mailBody.ts
@@ -0,0 +1,59 @@
+export const checkInSubjects = [
+  "Starting Work",
+  "On the Clock",
+  "Commencing Work",
+  "Ready to Roll",
+  "Morning Check-In",
+  "Quick Check-in",
+];
+
+export const signOutSubjects = [
+  "Wrapping Up",
+  "End of Day Report",
+  "Day's End",
+  "Closing Time",
+  "End of Shift",
+];
+
+export const checkInBodies = [
+  "Dear Yoritsune-san & Okubo-san,\n\nGood morning! Just a quick note to let you know that I've started my workday. I'm here and ready to tackle whatever tasks come our way today. Let me know if there's anything specific you need from me.",
+  `Dear Yoritsune-san & Okubo-san,\n\nGood morning! I'm all set to begin work for the day. If there's anything pressing or any updates you need from me, feel free to reach out. Let's make today productive!`,
+  `Dear Yoritsune-san & Okubo-san,\n\nI hope you are well. Just wanted to drop you a line to confirm that I'm officially on the clock. Let's crush it today!`,
+  `Dear Yoritsune-san & Okubo-san,\n\nI hope you're doing well. I wanted to inform you that I'm now online and ready to kick off my workday. If there's anything urgent or if you have any instructions for me, please let me know.`,
+  `Dear Yoritsune-san & Okubo-san,\n\nHow are you? Just a quick check-in to let you know that I'm officially online and ready to start my day. If there's anything you need assistance with or if there are any updates, feel free to drop me a message. Let's make today a productive one!`,
+];
+
+export const signOutBodies = [
+  `Dear Yoritsune-san & Okubo-san,\n\nGood afternoon! I'm signing off for the day. It's been a productive one, and I've made significant progress on our projects. If there's anything urgent before I log off, please let me know.`,
+  `Dear Yoritsune-san & Okubo-san,\n\nHope you had a wonderful day. Just a heads-up that I'm wrapping up my work for today. Let me know if you need anything else before I call it a day.`,
+  `Dear Yoritsune-san & Okubo-san,\n\nGood afternoon! I'm officially calling it a day. If there's anything you need from me before I sign off, please reach out. Otherwise, I'll catch up with you tomorrow.`,
+  `Dear Yoritsune-san & Okubo-san,\n\nI hope you had a great day. Just wanted to inform you that I'm finishing up for the day. If there's anything urgent, feel free to ping me; otherwise, I'll touch base with you tomorrow.`,
+  `Dear Yoritsune-san & Okubo-san,\n\nWrapping up my workday now. If there's anything else you need from me before I go, please let me know. Looking forward to picking up where we left off tomorrow.`,
+];
+
+export const MondaycheckInBodies = [
+  "Dear Yoritsune-san & Okubo-san,\nHappy Monday! Just a quick note to let you know that I've started my workday. I'm here and ready to tackle whatever tasks come our way today. Let me know if there's anything specific you need from me.",
+  `Dear Yoritsune-san & Okubo-san,\n\Happy Monday! I'm all set to begin work for the day. If there's anything pressing or any updates you need from me, feel free to reach out. Let's make first day of the week productive!`,
+  `Dear Yoritsune-san & Okubo-san,\n\nI hope you are well. Just wanted to drop you a line to confirm that I'm officially on the clock. Let's crush the first day of the week!`,
+  `Dear Yoritsune-san & Okubo-san,\n\nI hope you had a wonderful weekend. I wanted to inform you that I'm now online and ready to kick off my workday. If there's anything urgent or if you have any instructions for me, please let me know.`,
+  `Dear Yoritsune-san & Okubo-san,\n\nHow are you? Just a quick check-in to let you know that I'm officially online and ready to start my day. If there's anything you need assistance with or if there are any updates, feel free to drop me a message. Let's make today a productive one!`,
+];
+
+export const FridaysignOutBodies = [
+  `Dear Yoritsune-san & Okubo-san,\n\nGood afternoon! I'm signing off for the day. It's been a productive one. If there's anything urgent before I log off, please let me know.`,
+  `Dear Yoritsune-san & Okubo-san,\n\nHope you had a wonderful day. Just a heads-up that I'm wrapping up my work for today. Let me know if you need anything else before I call it a day.`,
+  `Dear Yoritsune-san & Okubo-san,\n\nGood afternoon! I'm officially calling it a day. If there's anything you need from me before I sign off, please reach out. Otherwise, I'll catch up with you on Monday.`,
+  `Dear Yoritsune-san & Okubo-san,\n\nI hope you had a great day. Just wanted to inform you that I'm finishing up for the day. If there's anything urgent, feel free to ping me; otherwise, I'll touch base with you next week.`,
+  `Dear Yoritsune-san & Okubo-san,\n\nWrapping up my workday now. If there's anything else you need from me before I go, please let me know. Looking forward to picking up where we left off on Monday.`,
+];
+
+export const signature = [
+  `Sincerly,\nLeah`,
+  `Best regards,\nLeah`,
+  "Regards,\nLeah",
+  "Warm Regards,\nLeah",
+  "Best,\nLeah",
+  "Until tomorrow,\nLeah",
+  "Kind Regards,\nLeah",
+  "Take Care,\nLeah",
+];
